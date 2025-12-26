1f3ba9ec725bbef7b220612c7860e3d2aac32a3e
Fernando Contreras <fernando.contreras2143@gmail.com>
2025-12-26 16:02:18 -0600

    refactor: refactor function to do mor eficient

--- insertion-sort/main.cpp
+++ insertion-sort/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-
+void printArray(int numbers[], int ln) {
+    for (int i = 0; i < ln; i++) {
+        std::cout << numbers[i];
+        if (i < ln -1) std::cout<<", ";
+    }
+}
 int main() {
-    int numbers[] = {5,9,1,3};
+    int numbers[] = {5,9,1,3, 4};
     int ln = std::size(numbers);
     for (int i = 1; i < ln ; i++) {
-        for (int j = i; j > 1; j--) {
-            if (numbers[j -1] > numbers[j] ) {
-                int aux  = numbers[j - 1];
-                numbers[j-1] = numbers[j];
-                numbers[j] = aux;
-            }
+        for (int j = i; j > 0 && (numbers[j - 1] > numbers[j]); j-- ) {
+                int aux  = numbers[j];
+                numbers[j] = numbers[j - 1];
+                numbers[j-1] = aux;
         }
     }
+    printArray(numbers, ln);
     return 0;
 }
