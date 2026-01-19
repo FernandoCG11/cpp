40ee3224320844799189f84208491eb561bf5770
FernandoCG11 <fernando.contreras2143@gmail.com>
2026-01-18 23:44:47 -0600

    feat: add data template to use it at quicksort

--- t-quicksort/main.cpp
+++ t-quicksort/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
-void printArray(int vec[],int ln) {
+template <typename T>
+void printArray(T vec[],int ln) {
     for (int i = 0; i< ln; i++) {
         std::cout << vec[i];
         if (i < ln -1 ) std::cout << ", ";
     }
 }
 
-int partition(int vec[], const int low, const int high) {
-    const int pivot = vec[high];
+template <typename T>
+int partition(T vec[], const int low, const int high) {
+    T pivot = vec[high];
     int i = low - 1 ;
     for (int j = low; j <= high - 1; j++) {
         if (vec[j] <= pivot) {
@@ -21,7 +23,8 @@ int partition(int vec[], const int low, const int high) {
     return i + 1;
 }
 
-void quickSort(int vec[], const int low, const int high) {
+template <typename T>
+void quickSort(T vec[], const int low, const int high) {
     if (low < high) {
         const int pi = partition(vec, low, high);
         quickSort(vec, low, pi - 1);
@@ -30,8 +33,8 @@ void quickSort(int vec[], const int low, const int high) {
 }
 
 int main() {
-    int numbers[] = {4,16,77, 45, 10,90,20,1};
-     int ln = std::size(numbers);
+    double numbers[] = {4,16,77, 45, 10,90,20,1};
+    constexpr int ln = std::size(numbers);
     quickSort(numbers, 0, ln - 1);
     printArray(numbers, ln);
     return 0;
