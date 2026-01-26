4b2bec43694c2644c1256df0f77522ff57a9844c
FernandoCG11 <fernando.contreras2143@gmail.com>
2026-01-25 20:57:31 -0600

    feat: define animal fields and behavoirs

--- clases/inheritance/Animal.cpp
+++ clases/inheritance/Animal.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "Animal.h"
+
+#include <iostream>
+#include <utility>
+
+Animal::Animal(std::string _scientificName, int _age, const std::string& _nativeRegion, std::string _color) : scientificName(std::move(_scientificName)), age(_age),  color(std::move(_color)){};
+
+void Animal::getName() {
+    std::cout << "Scientific name: " << scientificName;
+}
+
+void Animal::getAge() const {
+    std::cout << "Age: " << age;
+}
+
+void Animal::getNativeRegion() {
+    std::cout << "Native region: " << nativeRegion;
+}
+
+void Animal::getColor() {
+    std::cout << "Color: " << color;
+}
--- clases/inheritance/Animal.h
+++ clases/inheritance/Animal.h
@@ -4,9 +4,22 @@
 
 #ifndef INHERITANCE_ANIMAL_H
 #define INHERITANCE_ANIMAL_H
+#include <string>
 
 
 class Animal {
+private:
+    std::string scientificName;
+    int age = 0;
+    std::string nativeRegion;
+    std::string color;
+
+public:
+    Animal(std::string, int, const std::string&, std::string);
+    void getName();
+    void getAge() const;
+    void getNativeRegion();
+    void getColor();
 };
 
 
