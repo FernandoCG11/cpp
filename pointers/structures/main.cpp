feca7e68df4270c823cc4bea7f6a43f22cd9391d
FernandoCG11 <fernando.contreras2143@gmail.com>
2026-02-05 16:10:26 -0600

    feat: add example of structure pointer

--- pointers/structures/main.cpp
+++ pointers/structures/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
-int main() {
+struct Person {
+    std::string name;
+    int age = 0;
+};
+    Person person = {};
+    Person* ptr = &person;
 
+void enterValue() {
+    std::cout << "Enter name: ";
+    getline(std::cin, ptr->name);
+    std::cout << "Enter age: ";
+    std::cin >> ptr->age;
+}
+
+void printData() {
+    std::cout << "Name: " << ptr->name;
+    std::cout << "\nAge: " << ptr->age;
+
+}
+int main() {
+    enterValue();
+    printData();
     return 0;
 }
