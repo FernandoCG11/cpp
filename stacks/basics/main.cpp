ddca2238dfaa6581dd9409d347f30754ba581f42
FernandoCG11 <fernando.contreras2143@gmail.com>
2026-02-08 19:59:02 -0600

    feat: add stack base

--- stacks/basics/main.cpp
+++ stacks/basics/main.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 
 struct Node {
-    int data = 0 ;
-    Node *next{};
+    int value = 0;
+    Node* next{};
 };
 
-void addStack(Node *&stack, int n) {
-    Node *new_node = new Node();
-    new_node->data=n;
-    new_node->next = stack;
-    stack = new_node;
+void pushStack(Node* &stack, int n) {
+    Node *new_node = new Node(); // Create new stack
+    new_node->value = n;
+    new_node->next=stack;
+    stack =new_node;
+}
+
+auto popStack(Node* &stack, int &n) {
+    if (stack == nullptr) {
+        throw std::runtime_error("Stack is empty!");
+    }
+    Node* aux = stack;
+    n = stack->value;
+    stack = stack->next;
+    delete aux;
+    return n;
+}
+
+auto topStack(Node* &stack) {
+    return stack->value;
 }
 int main() {
-    Node *stack = nullptr;
-    int n;
-    std::cout << "Enter number: ";
-    std::cin >> n;
-    addStack(stack,n);
+    Node stack = {10, nullptr};
+    Node* ptrStack = &stack;
+    pushStack(ptrStack, 30);
+    pushStack(ptrStack, 20);
+    pushStack(ptrStack, 10);
+
+    std::cout << "Top stack: " << topStack(ptrStack);
     return 0;
 }
