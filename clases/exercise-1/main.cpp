4b979319f292b570b68c7eff5d9187458fd1c807
FernandoCG11 <fernando.contreras2143@gmail.com>
2026-03-16 16:45:00 -0600

    feat: add classes instances

--- clases/exercise-1/main.cpp
+++ clases/exercise-1/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <memory>
+
+#include "Date.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto myDate = std::make_unique<Date>(2004, "March", 13);
+    auto yourDate = std::make_unique<Date>(2004, "March", 13);
+    // Pass reference
+    if (myDate->equal(*myDate)) {
+        std::cout << "Coincide";
+    }
     return 0;
 }
