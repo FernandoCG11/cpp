4e5cc7d5cb011cd9e795d672aa0250f1673dc274
FernandoCG11 <fernando.contreras2143@gmail.com>
2026-03-25 21:38:26 -0600

    feat: add an public inheritance example

--- clases/inheritance-classes/main.cpp
+++ clases/inheritance-classes/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "Turism.h"
 #include "Vehicle.h"
 
 int main() {
+    Turism t1 {"Volvo", 2020, "Red", "McQueen"};
+    std::cout << t1.getBrand()<< std::endl;
     return 0;
 }
