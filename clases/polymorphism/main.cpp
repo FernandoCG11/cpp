a4a6754caacade125dfe2a64061760cb75d2f0c1
FernandoCG11 <fernando.contreras2143@gmail.com>
2026-04-04 14:05:08 -0600

    feat: add polymorphis example

--- clases/polymorphism/main.cpp
+++ clases/polymorphism/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+
+#include "Poligon.h"
+#include "Triangle.h"
+#include "cmake-build-debug/Rectangle.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<Poligon*> v1;
+    v1.push_back(new Rectangle(22,33));
+    v1.push_back(new Triangle(12,18));
+
+    for (Poligon*& v : v1) {
+        std::cout << "Area: " <<v->getArea() << std::endl;
+        std::cout << "Perimeter: " << v->getPerimeter() << std::endl;
+    };
     return 0;
 }
