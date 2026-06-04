18610563edd0b3938bea60ee1d2f3454552d9ea5
FernandoCG11 <fernando.contreras2143@gmail.com>
2026-06-03 23:20:44 -0600

    feat: add solution for two sum exercise

---
+++ leetcode/two-sum/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> map;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (map.count(complement)) {
+            return {map[complement], i};
+        }
+        map[nums[complement]] = i;
+    }
+    return {};
+}
+
+int main() {
+
+    return 0;
+}
